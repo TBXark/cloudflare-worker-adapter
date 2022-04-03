7bc12a112e732a5ab481a17e5248da8b64f59bf7
TBXark <tbxark@outlook.com>
2022-04-03 17:37:28 +0800

    add EventEmitter

--- index.js
+++ index.js
@@ -1,35 +1,15 @@
 import {Command} from 'commander';
 import http from 'http';
 import fetch, {Request, Response} from 'node-fetch';
+import EventEmitter from 'events';
+
+const globalEventEmitter = new EventEmitter();
 
 global.fetch = fetch;
 global.Request = Request;
 global.Response = Response;
-global.addEventListener = addEventListener;
-
-
-export function bindGlobal(target) {
-  Object.keys(target).forEach((key) => {
-    if (typeof target[key] === 'function') {
-      global[key] = target[key].bind(target);
-    } else {
-      global[key] = target[key];
-    }
-  });
-}
-
-
-const eventListenersStore = {};
-
-function addEventListener(type, listener) {
-  if (typeof listener !== 'function') {
-    throw new Error('listener must be a function');
-  }
-  if (!eventListenersStore[type]) {
-    eventListenersStore[type] = [];
-  }
-  eventListenersStore[type].push(listener);
-}
+global.addEventListener =
+  globalEventEmitter.addListener.bind(globalEventEmitter);
 
 function convertIncomeToNodeFecthRequest(income, host) {
   const {url, method, headers, body} = income;
@@ -64,48 +44,59 @@ export function startServer(port) {
   }
 
   function isPromise(obj) {
-    return !!obj && (typeof obj === 'object' || typeof obj === 'function') && typeof obj.then === 'function';
+    return (
+      !!obj &&
+      (typeof obj === 'object' || typeof obj === 'function') &&
+      typeof obj.then === 'function'
+    );
   }
 
-  const server = http.createServer((req, res) => {
-      const listeners = eventListenersStore['fetch'];
-        if (listeners) {
-            const event = {
-                request: convertIncomeToNodeFecthRequest(req, host),
-                respondWith: (response) => {
-                    if (isPromise(response)) {
-                        response.then((nodeFecthResponse) => {
-                            mergeNodeFecthResponseIntoOutcome(nodeFecthResponse, res);
-                        });
-                    } else {
-                        mergeNodeFecthResponseIntoOutcome(response, res);
-                    }
-                }
-            };
-            for (const listener of listeners) {
-                listener(event);
+  const server = http
+      .createServer((req, res) => {
+        const event = {
+          request: convertIncomeToNodeFecthRequest(req, host),
+          respondWith: (response) => {
+            if (isPromise(response)) {
+              response.then((data) => {
+                mergeNodeFecthResponseIntoOutcome(data, res);
+              });
+            } else {
+              mergeNodeFecthResponseIntoOutcome(response, res);
             }
-        }
-  }).listen(port);
+          },
+        };
+        globalEventEmitter.emit('fetch', event);
+      })
+      .listen(port);
 
   console.log(`Server running at ${host}:${port}/`);
   return server;
 }
 
+export function bindGlobal(target) {
+  Object.keys(target).forEach((key) => {
+    if (typeof target[key] === 'function') {
+      global[key] = target[key].bind(target);
+    } else {
+      global[key] = target[key];
+    }
+  });
+}
+
+
 export class MemoryCache {
-    constructor() {
-      this.cache = {};
-    }
-    async get(key) {
-      return this.cache[key];
-    }
-  
-    async put(key, value) {
-      this.cache[key] = value;
-    }
-  
-    async delete(key) {
-      delete this.cache[key];
-    }
+  constructor() {
+    this.cache = {};
   }
-  
+  async get(key) {
+    return this.cache[key];
+  }
+
+  async put(key, value) {
+    this.cache[key] = value;
+  }
+
+  async delete(key) {
+    delete this.cache[key];
+  }
+}
